d2d67e3b7361d1fd0b056e76958e056589c4db3e
Adrian <adrian.orenstein@gmail.com>
2020-03-30 14:44:44 +1030

    finished producer_consumer on environment

--- producer_consumer.py
+++ producer_consumer.py
@@ -36,6 +36,14 @@ class Memory:
 		del self.rewards[:]
 		del self.is_terminals[:]
 
+	def load_experiences(self, experiences: list):
+		for exp in experiences:
+			self.actions.append(exp['action']),
+			self.states.append(exp['state']),
+			self.logprobs.append(exp['action_logprob']),
+			self.rewards.append(exp['reward']),
+			self.is_terminals.append(exp['done']),
+
 
 class ActorCritic(nn.Module):
 	def __init__(self, state_dim, action_dim, action_std):
@@ -112,8 +120,8 @@ class PPO:
 		self.MseLoss = nn.MSELoss()
 	
 	def select_action(self, state):
-		state = torch.FloatTensor(state.reshape(1, -1)).to(device)
-		return (res.cpu().data.numpy().flatten() for res in self.policy_old.act(state))
+		state = torch.tensor(state).float().to(device)
+		return (res.cpu().data.numpy() for res in self.policy_old.act(state))
 	
 	def update(self, memory):
 		# Monte Carlo estimate of rewards:
@@ -129,21 +137,14 @@ class PPO:
 		# Normalizing the rewards:
 		rewards = torch.tensor(rewards).to(device)
 		rewards = (rewards - rewards.mean()) / (rewards.std() + 1e-5)
-		
+
 		# convert list to tensor
-		old_states = torch.squeeze(torch.stack(memory.states).to(device), 1).detach()
-		old_actions = torch.squeeze(torch.stack(memory.actions).to(device), 1).detach()
-		old_logprobs = torch.squeeze(torch.stack(memory.logprobs), 1).to(device).detach()
-
-		min_size = min(len(rewards), len(old_states), len(old_actions), len(old_logprobs))
-
-		rewards = rewards[:min_size]
-		old_states = old_states[:min_size]
-		old_actions = old_actions[:min_size]
-		old_logprobs = old_logprobs[:min_size]
+		old_states = torch.squeeze(torch.tensor(np.stack(memory.states)[:, np.newaxis, :],).float().to(device), 1).detach()
+		old_actions = torch.squeeze(torch.tensor(np.stack(memory.actions)[:, np.newaxis, :]).float().to(device), 1).detach()
+		old_logprobs = torch.squeeze(torch.tensor(np.stack(memory.logprobs)[:, np.newaxis, np.newaxis]).float(), 1).to(device).detach()
 		
-		tmp_loss = 0
 		# Optimize policy for K epochs:
+		tmp_loss = 0
 		for _ in range(self.K_epochs):
 			# Evaluating old actions and values :
 			logprobs, state_values, dist_entropy = self.policy.evaluate(old_states, old_actions)
@@ -173,23 +174,26 @@ import torch.multiprocessing as mp
 import ctypes
 import numpy as np
 
-
 # Parallel processing
 class Producer:
-	def __init__(self, worker_id, worker_ppo, hparams):
+	def __init__(self, worker_id, hparams, processing_queue):
 		self.id = worker_id
-		self.global_obs = dict()
-		self.ppo = worker_ppo
-		self.memory = Memory()
-
 		self.hparams = hparams
+
+		self.processing_queue = processing_queue
+		self.inbox_queue = mp.Queue()
+		self.experience_queue = mp.Queue()
+
+		self.global_obs = dict()
 		self.state = RingBuffer(capacity=self.hparams['state_dim'], dtype=np.float32)
 
-	def reset(self):
-		# instantiate buffer with all zeros
-		self.state.extend(np.zeros((self.hparams['state_dim'])))
+		self.process = mp.Process(
+			target=self.run
+		)
 
-		self.memory.clear_memory()
+	def reset(self):
+		# flush buffer with all zeros
+		self.state.extend(np.zeros((self.hparams['state_dim'])))
 
 		self.global_obs = {
 			'killCount':	0,
@@ -208,8 +212,6 @@ class Producer:
 		def parse_distances(arr: tuple):
 			return get_dist(arr[0], arr[1])
 
-		# print(observations)
-
 		if not observations:
 			# print("no observations.. there is no one around me?")
 			observations = {
@@ -221,7 +223,6 @@ class Producer:
 		# grab the 15 closest tanks
 		tank_locations = [ (float(tank_dic['x']), float(tank_dic['y']) ) for tank_dic in observations['radarScan']]
 		tank_locations = sorted(tank_locations, key=parse_distances)
-		# print([(parse_distances(dis), 1/parse_distances(dis)) for dis in tank_locations])
 
 		ret_obs = {
 			'killCount':	observations['killCount'],
@@ -235,87 +236,101 @@ class Producer:
 		for i, loc in enumerate(tank_locations):
 			ret_state[i] = loc
 
-		return ret_state, ret_obs, True if observations['deathCount'] >= 3 else False
+		return ret_state, ret_obs, False #True if observations['deathCount'] >= 3 else False
 
 	def run(self):
-		# clean up
-		self.reset()
+		try:
+			while True:
+				# clean up
+				self.reset()
 
-		obs = self.global_obs
+				obs = self.global_obs
 
-		# RlWorldClient("129.127.147.237", 1337)
-		env = RlWorldClient("10.90.159.11", 1337)
-		for t in range(self.hparams['max_timesteps']):
-			# try:
-			start_time = time.time()
+				# RlWorldClient("129.127.147.237", 1337)
+				env = RlWorldClient("10.90.159.11", 1337)
+				for t in range(self.hparams['max_timesteps']):
+					start_time = time.time()
 
-			state_current, obs, done = self.parse_observations(env.read_observation_dict(), obs)
-			self.state.extend(state_current.reshape(-1))
+					state_current, obs, done = self.parse_observations(env.read_observation_dict(), obs)
+					self.state.extend(state_current.reshape(-1))
 
-			action, action_logprob = self.ppo.select_action(np.array(self.state))
-			self.state.extend(action.reshape(-1))
+					# send state to processing queue
+					self.processing_queue.put({
+						'id': self.id, 
+						'state': np.array(self.state),
+					})
 
-			# calculate rewards
-			mm_clip = lambda x, l, u: max(l, min(u, x))
+					# get action response
+					response = self.inbox_queue.get()
+					# print(response)
+					if response is None:
+						print(f"Process {self.id} is shutting down..")
+						return
 
-			# Rewards
-			delta_kills = obs['killCount']	- self.global_obs['killCount']
-			delta_hits 	= obs['hitCount']	- self.global_obs['hitCount']
-			delta_death = obs['deathCount']	- self.global_obs['deathCount']
+					assert response['id'] == self.id
+					action = response['action']
+					action_logprob = response['action_logprob']
+					self.state.extend(action.reshape(-1))
 
-			time_discount = mm_clip((-1/(self.hparams['max_timesteps']*1.1)) * t + 1, 0, 1)
+					# calculate rewards
+					mm_clip = lambda x, l, u: max(l, min(u, x))
 
-			reward = [
-				5	 	* delta_kills * time_discount,
-				1    	* delta_hits * time_discount,
-			]
+					# Rewards
+					delta_kills = obs['killCount']	- self.global_obs['killCount']
+					delta_hits 	= obs['hitCount']	- self.global_obs['hitCount']
+					delta_death = obs['deathCount']	- self.global_obs['deathCount']
+
+					time_discount = mm_clip((-1/(self.hparams['max_timesteps']*1.1)) * t + 1, 0, 1)
+
+					reward = [
+						5	 	* delta_kills * time_discount,
+						1    	* delta_hits * time_discount,
+					]
+
+					# Send back experience asap
+					action = action.tolist()
+					xp = {
+						'id': self.id,
+						'state': np.array(self.state),
+						'action': action,
+						'action_logprob': action_logprob,
+						'reward': sum(reward),
+						'done': done
+					}
+					# print(xp)
+					self.experience_queue.put(xp)
+							
+					self.global_obs['killCount'] += delta_kills
+					self.global_obs['hitCount'] += delta_hits
+					self.global_obs['deathCount'] += delta_death
 					
-			self.global_obs['killCount'] += delta_kills
-			self.global_obs['hitCount'] += delta_hits
-			self.global_obs['deathCount'] += delta_death
+					
+					
+					# Send off action to environment
+					env.send_action_dict({
+						"name": f"swarm_id:{self.id}",
+						"colour": "#7017a1",
+						"moveForwardBack": 	action[0],
+						"moveRightLeft": 	action[1],
+						"turnRightLeft": 	action[2],
+						"fire": True,
+					})
 
-			
-			# Apply action to dict
-			action = [float(act) for act in action]
-			
-			action_dict = {
-				"name": f"swarm_id:{self.id}",
-				"colour": "#7017a1",
-				"moveForwardBack": 	action[0],
-				"moveRightLeft": 	action[1],
-				"turnRightLeft": 	action[2],
-				"fire": True,
-			}
+					if done:
+						death_count += 1
+						break
+					
+					# load balancing for the server 
+					time_diff = time.time()-start_time
+					time.sleep(0 if time_diff > self.hparams['tick_time'] else self.hparams['tick_time']-time_diff)
 
-			env.send_action_dict(action_dict)
-
-			# Saving all memory:
-			self.memory.states.append(self.state),
-			self.memory.actions.append(action),
-			self.memory.logprobs.append(action_logprob),
-			self.memory.rewards.append(sum(reward)),
-			self.memory.is_terminals.append(done),
-
-			if done:
-				death_count += 1
-				break
-			
-			# load balancing for the server 
-			end_time = time.time()
-			time_diff = end_time-start_time
-			time.sleep(0 if time_diff > self.hparams['tick_time'] else self.hparams['tick_time']-time_diff)
-
-			# except Exception as e:
-			# 	print(f"worker {self.id} caught exception: {e}")
-			# 	time.sleep(0.5)
-
-		return self.memory	
-
-def run_worker(bot_id, ppo, hparams):
-	producer = Producer(bot_id, ppo, hparams)
-	return producer.run()
+		except Exception as e:
+			print(f"worker {self.id} caught exception: {e}")
+			time.sleep(0.5)	
 
 def main():
+	mp.set_start_method('spawn')
+
 	############## Hyperparameters ##############
 	id = random.randint(0,1000)
 
@@ -324,14 +339,13 @@ def main():
 	env_name = f"{math.floor(time.time())}__id_{id}"
 	writer = SummaryWriter("./logs/"+env_name)
 	tick_time = 0.20
-	batch_size = 30 # number of experiences until we run PPO
 	max_episodes = 10000        # max training episodes
 	max_timesteps = int(15 * (1/tick_time))        # max actions in one episode
 	
 	
 	update_timestep = max_timesteps * 2 # update policy every n timesteps
 	action_std = 0.5            # constant std for action distribution (Multivariate Normal)
-	K_epochs = 10               # update policy for K epochs
+	K_epochs = 3               # update policy for K epochs
 	eps_clip = 0.2              # clip parameter for PPO
 	gamma = 0.99                # discount factor
 	
@@ -345,7 +359,10 @@ def main():
 	num_tanks = 15
 	action_dim = 3
 	state_dim = (num_tanks*2)*(num_captures)+(action_dim*num_captures) # 15 tanks with (x,y) * 3 captures
-	
+
+
+	num_producers = 20
+	consumer_batch_size = 32
 
 	print(
 		"state_dim:", state_dim, "\n",
@@ -361,41 +378,87 @@ def main():
 		np.random.seed(random_seed)
 	
 	memory = Memory()
-	mp.set_start_method('forkserver')
 	ppo = PPO(state_dim, action_dim, action_std, lr, betas, gamma, K_epochs, eps_clip)
+
+	hparams = {
+		'max_timesteps': max_timesteps,
+		'state_dim': state_dim,
+		'num_tanks': num_tanks,
+		'tick_time': tick_time
+	}
+
+	producers = {}
+	processing_queue = mp.Queue() 
+
+	# create producers as worker objects
+	for rank in range(num_producers):
+		producers[rank] = Producer(
+			rank, 
+			hparams,
+			processing_queue
+			)
+
+	# start prducers
+	for worker_id, worker in producers.items():
+		worker.process.start()
+	print("finished making producer(s)")
 		
 	# training loop
 	for i_episode in tqdm(range(1, max_episodes+1)):
+		experiences = []
+		jobs_done = 0
 
-		# create workers, give them access to ppo inference
-		num_workers = 8
+		while jobs_done <= update_timestep*num_producers:
+			jobs = []
 
-		hparams = {
-			'max_timesteps': max_timesteps,
-			'state_dim': state_dim,
-			'num_tanks': num_tanks,
-			'tick_time': tick_time
+			while (not processing_queue.empty() or len(jobs) == 0) and len(jobs) < consumer_batch_size:
+				jobs.append(processing_queue.get())
+
+			if jobs:
+				with torch.no_grad():
+					data = np.stack([dic['state'].reshape(1, -1) for dic in jobs])
+					# print(data.shape)
+					action, action_logprob = ppo.select_action(data)
+					# print('action', action.shape, action)
+					# print('action_logprob', action_logprob.shape, action_logprob)
+					for rank, p_action, p_action_logprob in zip((j['id'] for j in jobs), np.rollaxis(action, 0), np.rollaxis(action_logprob, 0)):
+						producers[rank].inbox_queue.put({
+							'id': rank,
+							'action': p_action[0],
+							'action_logprob': p_action_logprob[0],
+						})
+						experiences.append(producers[rank].experience_queue.get())
+
+					jobs_done += len(jobs)
+		
+		
+		# update PPO model once we get enough experiences
+		memory.clear_memory()
+		memory.load_experiences(experiences)
+		loss = ppo.update(memory)
+
+		stats = {
+			'rewards/avg_reward': np.stack(memory.rewards).mean(),
+			'debug/loss': loss.item(),
 		}
-
-		# bots = [Producer(bot_id, ppo, hparams) for bot_id in range(num_workers)]
-		processes = []
-		for rank in range(num_workers):
-			p = mp.Process(target=run_worker, args=(rank, ppo, hparams))
-			p.start()
-			processes.append(p)
-
-		for p in processes:
-			p.join()
-		# return memory
-
-		print(p)
-		break
-
-		# update PPO model once we reach Y batch size 
-
-
+		
 		# log episode average rewards, stats etc, debug dict
-	
+		print(f'Episode {i_episode} \t Avg reward: {stats["rewards/avg_reward"]:.3f}')
+
+		for stat_name, stat in stats.items():
+			writer.add_scalar(stat_name, stat, i_episode)
+
+		# torch.save(ppo.policy.state_dict(), "./weights/" + env_name + ".pt")
+
+	print(f"Closing {len(producers)} producers")
+	for worker_id, worker in producers.items():
+		worker.inbox_queue.put(None)
+
+	for worker_id, worker in producers.items():
+		worker.process.join()
+
+	print("All done")
+
 			
 if __name__ == '__main__':
 	main()
